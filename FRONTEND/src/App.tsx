5c09bec20d3a800568d5771a042db3053e34c84a
Manjeet Roy <ymanjeet7063@gmail.com>
2025-02-16 02:21:30 +0530

    Add Register and login button

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router'
 import './App.css'
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
   return (
     <div>
       <h1>University insights</h1>
+      <Link to="/login" className='btn'>Login</Link>
+      <Link to="/register">Register</Link>
     </div>
   )
 }
